1823f6a5f416818607178b80f50a91ff98658539
SughnenWeb3 <jforster460@gmail.com>
2025-03-26 20:35:58 +0100

    first commit msg

---
+++ script.js
@@ -0,0 +1,46 @@
+//Array methods
+//find
+// let a = 10
+// let b = 4
+// let c = 2
+// a/= c/b
+// console.log(a)
+// console.log(a%b)
+// TASK
+// let a = 100
+// let b =50
+// let c= 3
+// c += b 
+// console.log(c)
+// c -= b
+// console.log(c)
+// c *= b
+// console.log(c)
+// c /= b
+// console.log(c)
+// let minus = 100 - 3
+// console.log(minus)
+// let multiply = 100 * 3
+// console.log(multiply)
+// let modulus = 100 % 3
+// console.log(modulus)
+// let exp = 100 ** 3
+// console.log(exp)
+
+let age = 5
+// age = age + 1
+age -= 1
+// second instance
+let score = 20
+let deduct = 21
+score -= deduct
+console.log(score)
+console.log(`initial values: Score = ${score}, Rand = ${deduct}`) //printing multiple values in Javascript using back tick
+score -= deduct
+console.log(`First operation: score = ${score}, Rand = ${deduct}`) //passing variables into strings using template
+let result = score += deduct
+console.log(`Final operation: score = ${score}, Rand = ${deduct}`)
+console.log("Result:", result)
+
+
+ 
